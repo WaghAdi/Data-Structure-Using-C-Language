53ddbc9d186f70175e1bfd1ea3fb523d3f967b21
aditya <adityawagh537>
2021-08-28 16:39:06 +0530

    Insertion and Deletion functionality added

--- Linked List operations/l.c
+++ Linked List operations/l.c
@@ -4,22 +4,22 @@
 struct node
 {
     int data;
-    struct node *next;
+    struct node *next; //self refrential structure
 };
-
-struct node *createNode()
+struct node *creatNode()
 {
-    struct node *newnode = NULL;
     int value;
+    struct node *newnode = NULL;
     newnode = (struct node *)malloc(sizeof(struct node));
     if (newnode == NULL)
     {
-        printf("memory not allocated\n");
+        printf("Memory not allocated\n");
     }
     else
     {
-        printf("memory allocated\n");
-        printf("Enter data to be enter\n");
+        printf("Memory allocted succesfully\n");
+
+        printf("Enter value of node\n");
         scanf("%d", &value);
         newnode->data = value;
         newnode->next = NULL;
@@ -27,10 +27,10 @@ struct node *createNode()
     return newnode;
 }
 
-void createLinklist(struct node **head)
+void createLinkedList(struct node **head)
 {
     struct node *newnode = NULL;
-    newnode = createNode();
+    newnode = creatNode();
     if (*head == NULL)
     {
         *head = newnode;
@@ -46,62 +46,191 @@ void createLinklist(struct node **head)
         temp->next = newnode;
     }
 }
-/*
-NO NEED TO  PASS ADDRESS OF FIRST AS WE DON'T HAVE TO CHANGE ADDRESS OF FIRST WE JUST WANT TO TRAVERSE A LINKEDLIST
 
-
-void displayLinklist(struct node **head)
+void displayLinkedList(struct node *head)
 {
-    struct node *temp = *head;
-    while (temp != NULL)
+    if (head == NULL)
     {
-        printf("%d =>", temp->data);
-        temp = temp->next;
+        printf("Linked List is Empty\n");
     }
-
+    else
+    {
+        while (head != NULL)
+        {
+            printf("%d ->", head->data);
+            head = head->next;
+        }
+        printf("\n");
+    }
 }
 
-*/
-
-
-void displayLinklist(struct node *head)
+void addAtFirst(struct node **head)
 {
-
-    while (head != NULL)
+    struct node *newnode = NULL;
+    newnode = creatNode();
+    newnode->next = *head;
+    *head = newnode;
+}
+void insertAtPosition(struct node **head)
+{
+    int position;
+    printf("Enter Position\n");
+    scanf("%d", &position);
+    if (position == 1)
     {
-        printf("%d =>", head->data);
-        head = head->next;
+        addAtFirst(head);
     }
-    printf("\n");
+
+    else
+    {
+
+        int count = 0;
+        int flag = 0;
+        struct node *temp = NULL;
+        struct node *temp1 = NULL;
+        temp = *head;
+        while (temp != NULL)
+        {
+            count += 1;
+            if (count == position - 1)
+            {
+                flag = 1;
+                break;
+            }
+            temp = temp->next;
+        }
+        if (flag == 1 && temp != NULL)
+        {
+            struct node *newnode = NULL;
+            newnode = creatNode();
+            temp1 = temp->next;
+            temp->next = newnode;
+            ;
+            newnode->next = temp1;
+        }
+        else if (temp == NULL && flag == 0)
+        {
+            printf("Invalid Index to add node \n");
+        }
+    }
+}
+
+void deleteAtFirst(struct node **head)
+{
+    struct node *temp = NULL;
+    temp = *head;
+    *head = temp->next;
+    free(temp);
+    printf("Deleted Successfully\n");
+}
+void deleteAtLast(struct node **head)
+{
+    struct node *temp = NULL;
+    struct node *temp1 = NULL;
+    temp = *head;
+    while (temp->next != NULL)
+    {
+        temp1 = temp;
+        temp = temp->next;
+    }
+    temp1->next = NULL;
+    free(temp);
+}
+
+void deleteAtPosition(struct node **head)
+{
+    int position=0;
+    printf("Enter position\n");
+    scanf("%d",&position);
+    if (position == 1)
+    {
+        deleteAtFirst(head);
+    }
+    else
+    {
+        int count = 0;
+        int flag = 0;
+        struct node *temp = NULL;
+        struct node *temp2 = NULL;
+        struct node *temp3 = NULL;
+        temp=*head;
+        while (temp != NULL)
+        {
+            count += 1;
+            if (count == position - 1)
+            {
+                flag = 1;
+                break;
+            }
+            temp = temp->next;
+        }
+        if (flag == 1 && temp != NULL)
+        {
+            temp2=temp->next;
+            temp3=temp2->next;
+            temp->next=temp3;
+            free(temp2);
+        }
+        else if (temp == NULL && flag == 0)
+        {
+            printf("Invalid Index to delete node \n");
+        }
+    }
 }
 
 void main()
 {
-    struct node *first = NULL;
-    char ch;
+    printf("Welcome user!!!\n");
     int choice;
+    struct node *first = NULL;
     do
     {
-        printf("Enter your choice\n");
-        printf("1 Create LinkedList\n");
-        printf("2 Display LinkedList\n");
+
+        printf("1 Create Linked List\n");
+        printf("2 Display Linked List\n");
+        printf("3 Insert node at first\n");
+        printf("4 Insert node at Last \n");
+        printf("5 Insert node at any Position\n");
+        printf("6 Delete node at first\n");
+        printf("7 Delete node at Last \n");
+        printf("8 Delete node at any Position\n");
+        printf("0 Exit \n");
+        printf("Enter you choice\n");
         scanf("%d", &choice);
         switch (choice)
         {
         case 1:
-            createLinklist(&first);
+            createLinkedList(&first);
             break;
         case 2:
-            displayLinklist(first);
+            displayLinkedList(first);
+            break;
+        case 3:
+            addAtFirst(&first);
+            break;
+        case 4:
+            createLinkedList(&first);
+            break;
+        case 5:
+            insertAtPosition(&first);
+            break;
+
+        case 6:
+            deleteAtFirst(&first);
+            break;
+        case 7:
+            deleteAtLast(&first);
+            break;
+        case 8:
+            deleteAtPosition(&first);
             break;
 
         case 0:
-            printf("Thank you\n");
+            printf("Thank you user!!!\n");
             break;
         default:
             printf("Invalid choice\n");
             break;
         }
-
     } while (choice != 0);
 }
