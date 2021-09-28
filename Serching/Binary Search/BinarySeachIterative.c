ad8c75835686b536cab8fb109d7d10f82225d282
aditya <adityawagh537@gmail.com>
2021-09-28 07:28:56 +0530

    Comment added for better understanding

--- Serching/Binary Search/BinarySeachIterative.c
+++ Serching/Binary Search/BinarySeachIterative.c
@@ -5,22 +5,25 @@ void binarySearch(int a[], int low, int high, int search)
     mid = (low + high) / 2;
     while (low <= high)
     {
-
+        //condition to cheak element is equal to mid or not
         if (a[mid] == search)
         {
             printf("element found\n");
             return;
         }
+        //condition if element is smallar than mid element
         else if (a[mid] > search && search >= a[low])
         {
             high = mid - 1;
             mid = (low + high) / 2;
         }
+        //condition if element is greater than mid element
         else if (a[mid] < search && search <= a[high])
         {
             low = mid + 1;
             mid = (low + high) / 2;
         }
+        //element not found
         else
         {
             printf("element not found\n");
@@ -43,5 +46,5 @@ void main()
     scanf("%d", &search);
     int low = 0;
     int high = n - 1;
-    binarySearch(a, low, high, search);
+    binarySearch(a, low, high, search); //function call
 }
--- Serching/Binary Search/BinarySearchRecursive.c
+++ Serching/Binary Search/BinarySearchRecursive.c
@@ -1,20 +1,25 @@
-#include<stdio.h>
-void binarySearch(int a[],int low,int high,int search)
+#include <stdio.h>
+void binarySearch(int a[], int low, int high, int search)
 {
-    int mid=(low+high)/2;
-    if(a[mid]==search)
+    int mid = (low + high) / 2; //to divide array
+    //condition to cheak element is equal to mid or not
+    if (a[mid] == search)
     {
         printf("element found\n");
+
         return;
     }
-    else if(a[mid]>search && search >= a[low])
+    //condition if element is smallar than mid element
+    else if (a[mid] > search && search >= a[low])
     {
-        binarySearch(a,low,mid-1,search);
+        binarySearch(a, low, mid - 1, search);
     }
-    else if(a[mid]<search && search<=a[high])
+    //condition if element is greater than mid element
+    else if (a[mid] < search && search <= a[high])
     {
-        binarySearch(a,mid+1,high,search);
+        binarySearch(a, mid + 1, high, search);
     }
+    //element not found
     else
     {
         printf("element not found\n");
@@ -22,21 +27,19 @@ void binarySearch(int a[],int low,int high,int search)
     }
 }
 void main()
-{   
-    int n,search;
+{
+    int n, search;
     int a[100];
     printf("enter how many element want to add\n");
-   scanf("%d",&n);
-   printf("enter array element in sorted order\n");
-   for(int i=0;i<n;i++)
-   {
-       scanf("%d",&a[i]);
-   }
-   printf("Enter element to be serch");
-   scanf("%d",&search);
-   int low=0;
-   int high=n-1;
-   binarySearch(a,low,high,search);
-
-    
+    scanf("%d", &n);
+    printf("enter array element in sorted order\n");
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+    printf("Enter element to be serch"); //element to be search
+    scanf("%d", &search);
+    int low = 0;
+    int high = n - 1;
+    binarySearch(a, low, high, search); //function call
 }
