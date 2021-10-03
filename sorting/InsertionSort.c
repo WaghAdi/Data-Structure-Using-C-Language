1426ca8ca972a833db18d9b9f68739ed24b0845e
aditya <adityawagh537@gmail.com>
2021-10-03 17:24:56 +0530

    comment added for readiability

--- sorting/InsertionSort.c
+++ sorting/InsertionSort.c
@@ -5,18 +5,19 @@ topic:insertion Sort
 */
 
 #include <stdio.h>
-void insertionSort(int arr[], int n)
+void insertionSort(int arr[], int n) //function defination
 {
     for (int i = 1; i < n; i++)
     {
         int temp = arr[i];
         int empty = i;
+        //lopp finding finding correct place for current element
         while (empty > 0 && arr[empty - 1] > temp)
         {
             arr[empty] = arr[empty - 1];
             empty--;
         }
-        arr[empty] = temp;
+        arr[empty] = temp; //adding elemetn to appropriate position
     }
 }
 void main()
@@ -35,7 +36,7 @@ void main()
         printf("%d ", arr[i]);
     }
     printf("Sorted Array In Ascending Order Using Insertion Sort: ");
-    insertionSort(arr, n);
+    insertionSort(arr, n); //function call
     for (int i = 0; i < n; i++)
     {
         printf("%d ", arr[i]);
